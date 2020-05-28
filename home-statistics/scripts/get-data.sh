d5005eca6f1a9442f89c19804f64f74b679cd3a1
Max Gherman <max.gherman.dev@gmail.com>
2020-05-28 23:22:43 +1000

    Get data by date

--- home-statistics/scripts/get-data.sh
+++ home-statistics/scripts/get-data.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-PREFIX=$1
+PREFIX=$(date +'%b-%d')
 
 echo $PREFIX
 
-curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/population.json --output ./public/src/data/population.json
+curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/population.json --output ./src/data/population.json
 
-curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/nswpostcodes_final.json --output ./public/src/data/post-codes.json
+curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/nswpostcodes_final.json --output ./src/data/post-codes.json
 
 # curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_cases.json --output cases_old.json
 
-curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_tests.json --output ./public/src/data/tests-$PREFIX.json
+curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_tests.json --output ./src/data/tests-$PREFIX.json
 
-curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_Cases2.json --output ./public/src/data/cases-$PREFIX.json
+curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_Cases2.json --output ./src/data/cases-$PREFIX.json
 
 # curl https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_Cases2.json
 
